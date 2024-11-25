bad0d6832aac5258132651afb348800038f3330e
Mohamad Abo Hilal <moabohilal@gmail.com>
2024-11-25 17:30:25 +0100

    added finsihed tasks

--- group4/rockPaperSciesors.js
+++ group4/rockPaperSciesors.js
@@ -8,3 +8,36 @@
 // Example:
 // node rockPaperScissors.js rock
 // # Output: You chose rock. Computer chose scissors. You win!
+
+const rps = ["Rock", "Paper", "Scissors"];
+
+const computerChoice = rps[Math.floor(Math.random() * rps.length)];
+
+const args = process.argv.slice(2);
+
+let playerChoice = args[0];
+
+if (playerChoice) {
+  playerChoice =
+    playerChoice[0].toUpperCase() + playerChoice.slice(1).toLowerCase();
+}
+
+if (!rps.includes(playerChoice)) {
+  console.log("Not a valid input. Please choose Rock, Paper, or Scissors.");
+  process.exit(1);
+}
+
+console.log("Player chose: " + playerChoice);
+console.log("Computer chose: " + computerChoice);
+
+if (computerChoice === playerChoice) {
+  console.log("Draw");
+} else if (
+  (computerChoice === "Rock" && playerChoice === "Scissors") ||
+  (computerChoice === "Paper" && playerChoice === "Rock") ||
+  (computerChoice === "Scissors" && playerChoice === "Paper")
+) {
+  console.log("Computer wins");
+} else {
+  console.log("Player wins");
+}
