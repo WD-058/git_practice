f6709660ece44fc8cf123cb93d7dfb2582f94be0
Paul Kim <ppaauullkkiimm@gmail.com>
2024-11-26 16:55:31 +0100

    first commit

--- group3/englishToPigLatin.js
+++ group3/englishToPigLatin.js
@@ -1,14 +1,27 @@
-// Description: Create a program that translates English text to Pig Latin.
-// Requirements:
-// The program should take an English phrase as an input from process.argv.
-// Convert each word to Pig Latin:
-// If a word starts with a consonant and a vowel, put the first letter of the word at the end of the word and add “ay.”
-// Example: Happy = appyh + ay = appyhay
-// If a word starts with two consonants move the two consonants to the end of the word and add “ay.”
-// Example: Child = Ildch + ay = Ildchay
+// function
+// The program should take an English phrase as an input from process.argv
+// slice and join
+
+//input = process.argv.slice.join;
+
 // If a word starts with a vowel add the word “way” at the end of the word.
-// Example: Awesome = Awesome +way = Awesomeway
+
+/* vowels = ["a", "e", "i", "o", "u"];
+return
+  word + "way" */
+
+// If a word starts with a consonant and a vowel, put the first letter of the word at the end of the word and add “ay.”
+
+/* !==  ["a", "e", "i", "o", "u"]
+
+return word + "ay"; */
+
+// If a word starts with two consonants move the two consonants to the end of the word and add “ay.”
+
 // Output the translated phrase to the console.
+
 // Example:
 // node pigLatin.js "Pig Latin is hard to speak"
 // # Output: Igpay Atinlay isway ardhay otay eakspay
+
+console.log(pigLatinPhrase);
